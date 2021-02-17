927c36ce205a7edaf957db36e907b82963047aa3
Jonas Grünwald <code@jonasg.me>
2021-02-17 15:53:55 +0000

    Updates

---
+++ precache-manifest.0c0500ae8d26e417b8ce5f38aebc8719.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2c554e1e296bb4e4ccebf12f15caad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b9a0df31e89fa2a88a2",
+    "url": "./static/js/2.20ca4f9d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.20ca4f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c9d4fa1311a1c4dcf8",
+    "url": "./static/js/main.42502c20.chunk.js"
+  },
+  {
+    "revision": "acc91226e1b4b7cd71b4",
+    "url": "./static/js/runtime-main.4fde0f30.js"
+  }
+]);
